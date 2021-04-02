66ef3d97190d2c0d77dbbe5ba3150ce8bb34a157
Andres <agarisoain@me.com>
2021-04-02 16:23:25 +0200

    alo

---
+++ legoQuest/src/legoquest/LegoQuest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package legoquest;
+
+/**
+ *
+ * @author Andrés
+ */
+public class LegoQuest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("alo");
+    }
+    
+}
